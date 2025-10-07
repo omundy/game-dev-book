ba33306e3caa22986e03d0d7f884fe7792ae6b58
Owen Mundy <omundy@gmail.com>
2025-10-07 14:55:24 -0400

    Test for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,26 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'Game Dev Book',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/omundy/game-dev-book' }],
+            sidebar: [
+                {
+                    label: 'Home', link: './'
+                },
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Game UI', slug: 'guides/ui' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        }),
+    ],
 });
---
+++ src/content/docs/guides/ui.md
@@ -0,0 +1,11 @@
+---
+title: Game UI
+description: Learn how to create responsive UI in your game
+---
+
+Learn how to create responsive UI in your game
+
+## Lab
+
+1. Create a new Scene in a Unity project
+2. 
